d7b4141a27066ee89b82118a19e28b8614596327
Can Özaytekin <canozaytekin@hotmail.com>
2019-06-12 13:57:23 +0300

    ButtonEdit customize

---
+++ CAN.SMS.UI.Win/UserControls/Controls/MyButtonEdit.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Controls;
+
+namespace CAN.SMS.UI.Win.UserControls.Controls
+{
+    public class MyButtonEdit : ButtonEdit
+    {
+        public MyButtonEdit()
+        {
+            Properties.TextEditStyle = TextEditStyles.DisableTextEditor;
+            Properties.AppearanceFocused.BackColor = Color.LightCyan;
+        }
+
+        public override bool EnterMoveNextControl { get; set; } = true;
+        public string statusBarDescription { get; set; }
+        public string statusBarShortCut { get; set; }
+        public string statusBarShortCutDescription { get; set; }
+    }
+}
