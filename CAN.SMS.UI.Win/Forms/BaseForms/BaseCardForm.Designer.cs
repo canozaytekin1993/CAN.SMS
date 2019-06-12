0f4f8b3a3f9ede86a4e3fcfec14311697daf7ba2
Can Özaytekin <canozaytekin@hotmail.com>
2019-06-12 13:05:47 +0300

    Create Base Form

---
+++ CAN.SMS.UI.Win/Forms/BaseForms/BaseCardForm.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace CAN.SMS.UI.Win.Forms.BaseForms
+{
+    partial class BaseCardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.Utils.SuperToolTip superToolTip1 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem1 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem1 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.ToolTipSeparatorItem toolTipSeparatorItem1 = new DevExpress.Utils.ToolTipSeparatorItem();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem2 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.SuperToolTip superToolTip2 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem3 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem2 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.ToolTipSeparatorItem toolTipSeparatorItem2 = new DevExpress.Utils.ToolTipSeparatorItem();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem4 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.SuperToolTip superToolTip3 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem5 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem3 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.ToolTipSeparatorItem toolTipSeparatorItem3 = new DevExpress.Utils.ToolTipSeparatorItem();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem6 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.SuperToolTip superToolTip4 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem7 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem4 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.ToolTipSeparatorItem toolTipSeparatorItem4 = new DevExpress.Utils.ToolTipSeparatorItem();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem8 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.SuperToolTip superToolTip5 = new DevExpress.Utils.SuperToolTip();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem9 = new DevExpress.Utils.ToolTipTitleItem();
+            DevExpress.Utils.ToolTipItem toolTipItem5 = new DevExpress.Utils.ToolTipItem();
+            DevExpress.Utils.ToolTipSeparatorItem toolTipSeparatorItem5 = new DevExpress.Utils.ToolTipSeparatorItem();
+            DevExpress.Utils.ToolTipTitleItem toolTipTitleItem10 = new DevExpress.Utils.ToolTipTitleItem();
+            this.ribbonControl = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.btnNew = new DevExpress.XtraBars.BarButtonItem();
+            this.btnSave = new DevExpress.XtraBars.BarButtonItem();
+            this.btnBack = new DevExpress.XtraBars.BarButtonItem();
+            this.btnDelete = new DevExpress.XtraBars.BarButtonItem();
+            this.btnExit = new DevExpress.XtraBars.BarButtonItem();
+            this.statusBarInfo = new DevExpress.XtraBars.BarStaticItem();
+            this.statusShortCut = new DevExpress.XtraBars.BarStaticItem();
+            this.statusShortCutInfo = new DevExpress.XtraBars.BarStaticItem();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonStatusBar1 = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.ribbonPage2 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonControl
+            // 
+            this.ribbonControl.DrawGroupCaptions = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.DrawGroupsBorderMode = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.ExpandCollapseItem.Id = 0;
+            this.ribbonControl.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl.ExpandCollapseItem,
+            this.ribbonControl.SearchEditItem,
+            this.btnNew,
+            this.btnSave,
+            this.btnBack,
+            this.btnDelete,
+            this.btnExit,
+            this.statusBarInfo,
+            this.statusShortCut,
+            this.statusShortCutInfo});
+            this.ribbonControl.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl.MaxItemId = 9;
+            this.ribbonControl.Name = "ribbonControl";
+            this.ribbonControl.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbonControl.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.ShowCategoryInCaption = false;
+            this.ribbonControl.ShowDisplayOptionsMenuButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.ShowExpandCollapseButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.ShowMoreCommandsButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.ShowPageHeadersInFormCaption = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl.ShowPageHeadersMode = DevExpress.XtraBars.Ribbon.ShowPageHeadersMode.Hide;
+            this.ribbonControl.ShowQatLocationSelector = false;
+            this.ribbonControl.ShowToolbarCustomizeItem = false;
+            this.ribbonControl.Size = new System.Drawing.Size(572, 102);
+            this.ribbonControl.StatusBar = this.ribbonStatusBar1;
+            this.ribbonControl.Toolbar.ShowCustomizeItem = false;
+            this.ribbonControl.ToolbarLocation = DevExpress.XtraBars.Ribbon.RibbonQuickAccessToolbarLocation.Hidden;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Caption = "New";
+            this.btnNew.Id = 1;
+            this.btnNew.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.addfile_16x16;
+            this.btnNew.ImageOptions.LargeImage = global::CAN.SMS.UI.Win.Properties.Resources.addfile_32x32;
+            this.btnNew.Name = "btnNew";
+            toolTipTitleItem1.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.comment_32x32;
+            toolTipTitleItem1.Text = "( Ctrl+Insert)";
+            toolTipItem1.LeftIndent = 6;
+            toolTipItem1.Text = "New Card";
+            toolTipTitleItem2.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.suggestion_16x16;
+            toolTipTitleItem2.LeftIndent = 6;
+            toolTipTitleItem2.Text = "Opens form in new card insert mode";
+            superToolTip1.Items.Add(toolTipTitleItem1);
+            superToolTip1.Items.Add(toolTipItem1);
+            superToolTip1.Items.Add(toolTipSeparatorItem1);
+            superToolTip1.Items.Add(toolTipTitleItem2);
+            this.btnNew.SuperTip = superToolTip1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Caption = "Save";
+            this.btnSave.Id = 2;
+            this.btnSave.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.save_16x16;
+            this.btnSave.ImageOptions.LargeImage = global::CAN.SMS.UI.Win.Properties.Resources.save_32x32;
+            this.btnSave.ItemShortcut = new DevExpress.XtraBars.BarShortcut(System.Windows.Forms.Keys.F2);
+            this.btnSave.Name = "btnSave";
+            toolTipTitleItem3.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.comment_32x32;
+            toolTipTitleItem3.Text = "( F2 )";
+            toolTipItem2.LeftIndent = 6;
+            toolTipItem2.Text = "Save";
+            toolTipTitleItem4.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.suggestion_16x16;
+            toolTipTitleItem4.LeftIndent = 6;
+            toolTipTitleItem4.Text = "Changes to the form are saved to the database with your approval";
+            superToolTip2.Items.Add(toolTipTitleItem3);
+            superToolTip2.Items.Add(toolTipItem2);
+            superToolTip2.Items.Add(toolTipSeparatorItem2);
+            superToolTip2.Items.Add(toolTipTitleItem4);
+            this.btnSave.SuperTip = superToolTip2;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Caption = "Back";
+            this.btnBack.Id = 3;
+            this.btnBack.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.reset_16x16;
+            this.btnBack.ImageOptions.LargeImage = global::CAN.SMS.UI.Win.Properties.Resources.reset_32x32;
+            this.btnBack.Name = "btnBack";
+            toolTipTitleItem5.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.comment_32x32;
+            toolTipTitleItem5.Text = "( Ctrl + Z )";
+            toolTipItem3.LeftIndent = 6;
+            toolTipItem3.Text = "Back";
+            toolTipTitleItem6.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.suggestion_16x16;
+            toolTipTitleItem6.LeftIndent = 6;
+            toolTipTitleItem6.Text = "Changes to the data in the form are undone and the opening data is loaded";
+            superToolTip3.Items.Add(toolTipTitleItem5);
+            superToolTip3.Items.Add(toolTipItem3);
+            superToolTip3.Items.Add(toolTipSeparatorItem3);
+            superToolTip3.Items.Add(toolTipTitleItem6);
+            this.btnBack.SuperTip = superToolTip3;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Caption = "Delete";
+            this.btnDelete.Id = 4;
+            this.btnDelete.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.deletelist_16x16;
+            this.btnDelete.ImageOptions.LargeImage = global::CAN.SMS.UI.Win.Properties.Resources.deletelist_32x32;
+            this.btnDelete.Name = "btnDelete";
+            toolTipTitleItem7.Text = "( Ctrl+Delete )";
+            toolTipItem4.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.comment_32x32;
+            toolTipItem4.LeftIndent = 6;
+            toolTipItem4.Text = "Delete";
+            toolTipTitleItem8.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.suggestion_16x16;
+            toolTipTitleItem8.LeftIndent = 6;
+            toolTipTitleItem8.Text = "The record you are working on is deleted from the database with your confirmation" +
+    "";
+            superToolTip4.Items.Add(toolTipTitleItem7);
+            superToolTip4.Items.Add(toolTipItem4);
+            superToolTip4.Items.Add(toolTipSeparatorItem4);
+            superToolTip4.Items.Add(toolTipTitleItem8);
+            this.btnDelete.SuperTip = superToolTip4;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Caption = "Exit";
+            this.btnExit.Id = 5;
+            this.btnExit.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.close_16x16;
+            this.btnExit.ImageOptions.LargeImage = global::CAN.SMS.UI.Win.Properties.Resources.close_32x32;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.ShortcutKeyDisplayString = "Esc";
+            toolTipTitleItem9.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.comment_32x32;
+            toolTipTitleItem9.Text = "( Esc )";
+            toolTipItem5.LeftIndent = 6;
+            toolTipItem5.Text = "Exit";
+            toolTipTitleItem10.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.suggestion_16x16;
+            toolTipTitleItem10.LeftIndent = 6;
+            toolTipTitleItem10.Text = "Form is closed with your approval";
+            superToolTip5.Items.Add(toolTipTitleItem9);
+            superToolTip5.Items.Add(toolTipItem5);
+            superToolTip5.Items.Add(toolTipSeparatorItem5);
+            superToolTip5.Items.Add(toolTipTitleItem10);
+            this.btnExit.SuperTip = superToolTip5;
+            // 
+            // statusBarInfo
+            // 
+            this.statusBarInfo.Id = 6;
+            this.statusBarInfo.ImageOptions.Image = global::CAN.SMS.UI.Win.Properties.Resources.suggestion_16x16;
+            this.statusBarInfo.ImageOptions.LargeImage = global::CAN.SMS.UI.Win.Properties.Resources.suggestion_32x32;
+            this.statusBarInfo.Name = "statusBarInfo";
+            this.statusBarInfo.PaintStyle = DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph;
+            // 
+            // statusShortCut
+            // 
+            this.statusShortCut.Alignment = DevExpress.XtraBars.BarItemLinkAlignment.Right;
+            this.statusShortCut.Id = 7;
+            this.statusShortCut.Name = "statusShortCut";
+            // 
+            // statusShortCutInfo
+            // 
+            this.statusShortCutInfo.Alignment = DevExpress.XtraBars.BarItemLinkAlignment.Right;
+            this.statusShortCutInfo.Id = 8;
+            this.statusShortCutInfo.Name = "statusShortCutInfo";
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "ribbonPage1";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnNew);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnSave);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnBack);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnDelete);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnExit);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+            // 
+            // ribbonStatusBar1
+            // 
+            this.ribbonStatusBar1.ItemLinks.Add(this.statusBarInfo);
+            this.ribbonStatusBar1.ItemLinks.Add(this.statusShortCut);
+            this.ribbonStatusBar1.ItemLinks.Add(this.statusShortCutInfo);
+            this.ribbonStatusBar1.Location = new System.Drawing.Point(0, 421);
+            this.ribbonStatusBar1.Name = "ribbonStatusBar1";
+            this.ribbonStatusBar1.Ribbon = this.ribbonControl;
+            this.ribbonStatusBar1.Size = new System.Drawing.Size(572, 31);
+            // 
+            // ribbonPage2
+            // 
+            this.ribbonPage2.Name = "ribbonPage2";
+            this.ribbonPage2.Text = "ribbonPage2";
+            // 
+            // BaseCardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 452);
+            this.Controls.Add(this.ribbonStatusBar1);
+            this.Controls.Add(this.ribbonControl);
+            this.MinimizeBox = false;
+            this.Name = "BaseCardForm";
+            this.Ribbon = this.ribbonControl;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StatusBar = this.ribbonStatusBar1;
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage2;
+        private DevExpress.XtraBars.BarButtonItem btnNew;
+        private DevExpress.XtraBars.BarButtonItem btnSave;
+        private DevExpress.XtraBars.BarButtonItem btnBack;
+        private DevExpress.XtraBars.BarButtonItem btnDelete;
+        private DevExpress.XtraBars.BarButtonItem btnExit;
+        private DevExpress.XtraBars.BarStaticItem statusBarInfo;
+        private DevExpress.XtraBars.BarStaticItem statusShortCut;
+        private DevExpress.XtraBars.BarStaticItem statusShortCutInfo;
+    }
+}
---
+++ CAN.SMS.UI.Win/Forms/BaseForms/BaseCardsForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace CAN.SMS.UI.Win.Forms.BaseForms
+{
+    public partial class BaseCardsForm : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public BaseCardsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
