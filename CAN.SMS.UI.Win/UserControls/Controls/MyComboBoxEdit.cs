9b09b0fb51f51d9b9c491ef85d11dae685495b8a
Can Özaytekin <canozaytekin@hotmail.com>
2019-06-12 22:24:28 +0300

    MyComboboxEdit create class

---
+++ CAN.SMS.UI.Win/UserControls/Controls/MyComboBoxEdit.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using CAN.SMS.UI.Win.Interfaces;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Controls;
+
+namespace CAN.SMS.UI.Win.UserControls.Controls
+{
+    public class MyComboBoxEdit : ComboBoxEdit, IStatusBarShortCut
+    {
+        [ToolboxItem(true)]
+        public MyComboBoxEdit()
+        {
+            Properties.AppearanceFocused.BackColor = Color.LightCyan;
+            Properties.TextEditStyle = TextEditStyles.DisableTextEditor;
+        }
+
+        public override bool EnterMoveNextControl { get; set; } = true;
+        public string statusBarDescription { get; set; }
+        public string statusBarShortCut { get; set; } = "F4 :";
+        public string statusBarShortCutDescription { get; set; }
+    }
+}
