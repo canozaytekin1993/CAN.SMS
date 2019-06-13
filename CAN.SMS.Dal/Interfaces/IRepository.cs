8adbdbb2ee8b25ec935de68771a0ce992a76ad19
Can Özaytekin <canozaytekin@hotmail.com>
2019-06-13 16:01:31 +0300

    Interfaces create folder

---
+++ CAN.SMS.Dal/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CAN.SMS.Dal.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        void Insert(T entity);
+        void Insert(IEnumerable<T> entities);
+        void Update(T entity);
+        void Update(T entity, IEnumerable<string> field);
+        void Update(IEnumerable<T> entities);
+        void Delete(T entity);
+        void Delete(IEnumerable<T> entities);
+        TResult Find<TResult>(Expression<Func<T, bool>> filter);
+        IQueryable<TResult> Select<TResult>(Expression<Func<T, bool>> filter);
+    }
+}
