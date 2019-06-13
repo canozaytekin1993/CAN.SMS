75194865408a2a1f99367b93437abc4ba4220b32
Can Özaytekin <canozaytekin@hotmail.com>
2019-06-13 21:37:05 +0300

    SchoolBll created.

--- CAN.SMS.Bll/General/SchoolBll.cs
+++ CAN.SMS.Bll/General/SchoolBll.cs
@@ -1,7 +1,13 @@
-﻿using System.Windows.Forms;
-using CAN.SMS.Bll.Base;
+﻿using CAN.SMS.Bll.Base;
+using CAN.SMS.Common.Enums;
 using CAN.SMS.Data.Contexts;
+using CAN.SMS.Model.Dto;
 using CAN.SMS.Model.Entities;
+using CAN.SMS.Model.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Windows.Forms;
 
 namespace CAN.SMS.Bll.General
 {
@@ -13,6 +19,50 @@ namespace CAN.SMS.Bll.General
 
         protected SchoolBll(Control ctrl) : base(ctrl)
         {
+        }
+
+        public BaseEntity Single(Expression<Func<School, bool>> filter)
+        {
+            return BaseSingle(filter, x => new SchoolS
+            {
+                Id = x.Id,
+                Code = x.Code,
+                SchoolName = x.SchoolName,
+                CountryId = x.CountryId,
+                CountryName = x.Country.CountryName,
+                CountyId = x.CountyId,
+                CountyName = x.County.CountyName,
+                Description = x.Description,
+                Statu = x.Statu
+            });
+        }
+
+        public IEnumerable<BaseEntity> List(Expression<Func<School, bool>> filter)
+        {
+            return BaseList(filter, x => new SchoolL
+            {
+                Id = x.Id,
+                Code = x.Code,
+                SchoolName = x.SchoolName,
+                CountryName = x.Country.CountryName,
+                CountyName = x.County.CountyName,
+                Description = x.Description
+            });
+        }
+
+        public bool Insert(BaseEntity entity)
+        {
+            return BaseInsert(entity, x => x.Code == entity.Code);
+        }
+
+        public bool Update(BaseEntity oldEntity, BaseEntity currentEntity)
+        {
+            return BaseUpdate(oldEntity, currentEntity, x => x.Code == currentEntity.Code);
+        }
+
+        public bool Delete(BaseEntity entity)
+        {
+            return BaseDelete(entity, CardType.School);
         }
     }
 }
---
+++ CAN.SMS.Model/Dto/SchoolDto.cs
@@ -0,0 +1,20 @@
+﻿using CAN.SMS.Model.Entities;
+using CAN.SMS.Model.Entities.Base;
+
+namespace CAN.SMS.Model.Dto
+{
+    // Attribute
+    public class SchoolS : School
+    {
+        public string CountryName { get; set; }
+        public string CountyName { get; set; }
+    }
+
+    public class SchoolL : BaseEntity
+    {
+        public string SchoolName { get; set; }
+        public string CountryName { get; set; }
+        public string CountyName { get; set; }
+        public string Description { get; set; }
+    }
+}
